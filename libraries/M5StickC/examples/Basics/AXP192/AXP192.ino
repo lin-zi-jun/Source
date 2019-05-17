d4f3d2e45366ee2627470d522549826056742865
lin-zi-jun <857413240@qq.com>
2019-05-17 14:21:56 +0800

    init

---
+++ libraries/M5StickC/examples/Basics/AXP192/AXP192.ino
@@ -0,0 +1,35 @@
+#include <M5StickC.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  M5.begin();
+  M5.Lcd.fillScreen(BLACK);
+  
+  M5.Axp.EnableCoulombcounter();
+}
+double vbat = 0.0;
+int discharge,charge;
+double temp = 0.0;
+double bat_p = 0.0;
+
+void loop() {
+
+  vbat      = M5.Axp.GetVbatData() * 1.1 / 1000;
+  charge    = M5.Axp.GetIchargeData() / 2;
+  discharge = M5.Axp.GetIdischargeData() / 2;
+  temp      =  -144.7 + M5.Axp.GetTempData() * 0.1;
+  bat_p     =  M5.Axp.GetPowerbatData() * 1.1 * 0.5 /1000;
+  
+  M5.Lcd.setCursor(0, 0, 1);
+  M5.Lcd.printf("vbat:%.3fV\r\n",vbat);
+  M5.Lcd.printf("icharge:%dmA\r\n",charge);
+  M5.Lcd.printf("idischg:%dmA\r\n",discharge);
+  M5.Lcd.printf("temp:%.1fC\r\n",temp);
+  M5.Lcd.printf("pbat:%.3fmW\r\n",bat_p);
+  M5.Lcd.printf("CoIn :%d\r\n",M5.Axp.GetCoulombchargeData());
+  M5.Lcd.printf("CoOut:%d\r\n",M5.Axp.GetCoulombdischargeData());
+  M5.Lcd.printf("CoD:%.2fmAh\r\n",M5.Axp.GetCoulombData());
+  M5.Lcd.printf("Vin:%.3fmV\r\n",M5.Axp.GetVinData() * 1.7);
+  M5.Lcd.printf("Iin:%.3fmA\r\n",M5.Axp.GetIinData() * 0.625);
+  delay(1000);
+}
---
+++ libraries/M5StickC/examples/Basics/SH200I/SH200I.ino
@@ -0,0 +1,46 @@
+#include <M5StickC.h>
+
+int16_t accX = 0;
+int16_t accY = 0;
+int16_t accZ = 0;
+
+int16_t gyroX = 0;
+int16_t gyroY = 0;
+int16_t gyroZ = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  M5.begin();
+  M5.IMU.Init();
+  M5.Lcd.setRotation(3);
+  M5.Lcd.fillScreen(BLACK);
+  M5.Lcd.setTextSize(1);
+  M5.Lcd.setCursor(40, 0);
+  M5.Lcd.println("SH200I TEST");
+  M5.Lcd.setCursor(0, 15);
+  M5.Lcd.println("  X       Y       Z");
+}
+
+
+
+int16_t temp = 0;
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  M5.IMU.getGyroData(&gyroX,&gyroY,&gyroZ);
+  M5.IMU.getAccelData(&accX,&accY,&accZ);
+  M5.IMU.getTempData(&temp);
+  
+  M5.Lcd.setCursor(0, 30);
+  M5.Lcd.printf("%.2f   %.2f   %.2f      ", ((float) gyroX) * M5.IMU.gRes, ((float) gyroY) * M5.IMU.gRes,((float) gyroZ) * M5.IMU.gRes);
+  M5.Lcd.setCursor(140, 30);
+  M5.Lcd.print("mg");
+  M5.Lcd.setCursor(0, 45);
+  M5.Lcd.printf("%.2f   %.2f   %.2f      ",((float) accX) * M5.IMU.aRes,((float) accY) * M5.IMU.aRes, ((float) accZ) * M5.IMU.aRes);
+  M5.Lcd.setCursor(140, 45);
+  M5.Lcd.print("o/s");
+  M5.Lcd.setCursor(0, 60);
+  M5.Lcd.printf("Temperature : %.2f C",((float) temp) / 333.87 + 21.0);
+  delay(1000);
+
+}
